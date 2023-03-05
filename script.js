99c7d29a075299383eeb470e31cfabb5a3c00956
Geeru4 <111010523+Geeru4@users.noreply.github.com>
2023-03-05 15:38:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-window.addEventListener("beforeunload",(event)=>{
-    const username = document.querySelector("#username");
-    const email = document.querySelector("#email");
-    const password = document.querySelector("#password");
+window.addEventListener("beforeunload",(event)=>{let username = document.querySelector("#username");let email = document.querySelector("#email");let password = document.querySelector("#password");
     window.localStorage.setItem("username",username.value);
     window.localStorage.setItem("email",email.value);
     window.localStorage.setItem("password",password.value);  
 })
-document.addEventListener("DOMContentLoaded",(event)=>{
-    const username = document.querySelector("#username");
-    const email = document.querySelector("#email");
-    const password = document.querySelector("#password");
+document.addEventListener("DOMContentLoaded",(event)=>{let username = document.querySelector("#username");let email = document.querySelector("#email");let password = document.querySelector("#password");
     username.value = window.localStorage.getItem("username");
     email.value = window.localStorage.getItem("email");
     password.value = window.localStorage.getItem("password");
